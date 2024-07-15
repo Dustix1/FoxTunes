827c1b30b7b4668c39cb74a7afcdd4f00851e412
dustix1 <davidcz2016@gmail.com>
2024-07-15 22:47:50 +0200

    added guildleave command for superuser

---
+++ src/commands/message/leaveGuild.ts
@@ -0,0 +1,20 @@
+import { Message, EmbedBuilder } from "discord.js";
+import Keys from "../../keys.js";
+import { CommandMessage } from "../../structures/command.js";
+import client from "../../clientLogin.js";
+
+export const command: CommandMessage = {
+    slash: false,
+    name: 'leaveguild',
+    usage: '\`\`!leaveguild\nNo available arguments.\`\`',
+    description: 'Leave a guild.',
+    group: 'general',
+    hidden: true,
+    async execute(message: Message, args: any) {
+        if (message.author.id !== Keys.ownerID) return;
+        const guild = client.guilds.cache.get(args[0]);
+        if (!guild) return message.channel.send('Guild not found.');
+        await guild.leave();
+        message.channel.send(`Left guild: ${guild.name}`);
+    }
+}
